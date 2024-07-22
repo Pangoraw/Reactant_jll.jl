694b88f0c0a8d40baaed4007698cab878f2f0151
jlbuild <juliabuildbot@gmail.com>
2024-07-22 10:19:34 +0000

    Reactant_jll build 0.0.12+2

--- .pkg/platform_augmentation.jl
+++ .pkg/platform_augmentation.jl
@@ -1,79 +1,27 @@
 using Base.BinaryPlatforms
 
-const Reactant_UUID = Base.UUID("3c362404-f566-11ee-1572-e11a4b42c853")
+const Reactant_UUID = Base.UUID("0192cb87-2b54-54ad-80e0-3be72ad8a3c0")
 const preferences = Base.get_preferences(Reactant_UUID)
+Base.record_compiletime_preference(Reactant_UUID, "mode")
+const mode_preference = if haskey(preferences, "mode")
+    if isa(preferences["mode"], String) && preferences["mode"] in ["opt", "dbg"]
+        preferences["mode"]
+    else
+        @error "Mode preference is not valid; expected 'opt' or 'dbg', but got '$(preferences["debug"])'"
+        nothing
+    end
+else
+    nothing
+end
 
 module __CUDA
-    using Base.BinaryPlatforms
-
-try
-    using CUDA_Runtime_jll
-catch
-    # during initial package installation, CUDA_Runtime_jll may not be available.
-    # in that case, we just won't select an artifact.
-end
-
-# can't use Preferences for the same reason
-const CUDA_Runtime_jll_uuid = Base.UUID("76a88914-d11a-5bdc-97e0-2f5a05c973a2")
-const preferences = Base.get_preferences(CUDA_Runtime_jll_uuid)
-Base.record_compiletime_preference(CUDA_Runtime_jll_uuid, "version")
-Base.record_compiletime_preference(CUDA_Runtime_jll_uuid, "local")
-const local_toolkit = something(tryparse(Bool, get(preferences, "local", "false")), false)
-
-function cuda_comparison_strategy(_a::String, _b::String, a_requested::Bool, b_requested::Bool)
-    # if we're using a local toolkit, we can't use artifacts
-    if local_toolkit
-        return false
-    end
-
-    # if either isn't a version number (e.g. "none"), perform a simple equality check
-    a = tryparse(VersionNumber, _a)
-    b = tryparse(VersionNumber, _b)
-    if a === nothing || b === nothing
-        return _a == _b
-    end
-
-    # if both b and a requested, then we fall back to equality
-    if a_requested && b_requested
-        return Base.thisminor(a) == Base.thisminor(b)
-    end
-
-    # otherwise, do the comparison between the the single version cap and the single version:
-    function is_compatible(artifact::VersionNumber, host::VersionNumber)
-        if host >= v"11.0"
-            # enhanced compatibility, semver-style
-            artifact.major == host.major &&
-            Base.thisminor(artifact) <= Base.thisminor(host)
-        else
-            Base.thisminor(artifact) == Base.thisminor(host)
-        end
-    end
-    if a_requested
-        is_compatible(b, a)
-    else
-        is_compatible(a, b)
-    end
-end
-
-function augment_platform!(platform::Platform)
-    if !@isdefined(CUDA_Runtime_jll)
-        # don't set to nothing or Pkg will download any artifact
-        platform["cuda"] = "none"
-    end
-
-    if !haskey(platform, "cuda")
-        CUDA_Runtime_jll.augment_platform!(platform)
-    end
-    BinaryPlatforms.set_compare_strategy!(platform, "cuda", cuda_comparison_strategy)
-
-    return platform
-end
+    $(CUDA.augment::String)
 end
 
 function augment_platform!(platform::Platform)
     __CUDA.augment_platform!(platform)
 
-    mode = get(ENV, "REACTANT_MODE", get(preferences, "mode", "opt"))
+    mode = get(ENV, "REACTANT_MODE", something(mode_preference, "opt"))
     if !haskey(platform, "mode")
         platform["mode"] = mode
     end
