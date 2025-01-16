1ef633697ef93964f00b0765762736a24a8ae869
jlbuild <juliabuildbot@gmail.com>
2025-01-16 16:48:38 +0000

    Reactant_jll build 0.0.41+2

--- .pkg/platform_augmentation.jl
+++ .pkg/platform_augmentation.jl
@@ -83,7 +83,7 @@ function augment_platform!(platform::Platform)
                     Base.include_dependency(path)
                     gpu = "cuda"
                 else
-                    @debug "CUDA version $(cuda_version) in $(path) not supported with this version of Reactant (min: $(min_cuda_version)"
+                    @debug "CUDA version $(cuda_version) in $(path) not supported with this version of Reactant (min: $(min_cuda_version))"
                 end
             end
         end
