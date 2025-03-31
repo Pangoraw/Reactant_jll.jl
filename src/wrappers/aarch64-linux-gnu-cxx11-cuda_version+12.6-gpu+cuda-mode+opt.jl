785923dc8b3e9a890b0fa6c3ddddb3c1cc48205d
jlbuild <juliabuildbot@gmail.com>
2025-03-31 05:22:44 +0000

    Reactant_jll build 0.0.112+0

--- src/wrappers/aarch64-linux-gnu-cxx11-cuda_version+12.6-gpu+cuda-mode+opt.jl
+++ src/wrappers/aarch64-linux-gnu-cxx11-cuda_version+12.6-gpu+cuda-mode+opt.jl
@@ -18,12 +18,12 @@ JLLWrappers.@declare_library_product(libcudnn_ops, "libcudnn_ops.so.9")
 JLLWrappers.@declare_library_product(libcufft, "libcufft.so.11")
 JLLWrappers.@declare_library_product(libcusolver, "libcusolver.so.11")
 JLLWrappers.@declare_library_product(libcusparse, "libcusparse.so.12")
-JLLWrappers.@declare_file_product(libdevice)
 JLLWrappers.@declare_library_product(libnccl, "libnccl.so.2")
 JLLWrappers.@declare_library_product(libnvJitLink, "libnvJitLink.so.12")
-JLLWrappers.@declare_library_product(libnvrtc, "libnvrtc.so.12")
 JLLWrappers.@declare_library_product(libnvrtc_builtins, "libnvrtc-builtins.so.12.6")
 JLLWrappers.@declare_executable_product(fatbinary)
+JLLWrappers.@declare_file_product(libdevice)
+JLLWrappers.@declare_library_product(libnvrtc, "libnvrtc.so.12")
 JLLWrappers.@declare_executable_product(ptxas)
 function __init__()
     JLLWrappers.@generate_init_header(CUDA_Driver_jll)
@@ -117,11 +117,6 @@ function __init__()
         nothing,
     )
 
-    JLLWrappers.@init_file_product(
-        libdevice,
-        "lib/cuda/nvvm/libdevice/libdevice.10.bc",
-    )
-
     JLLWrappers.@init_library_product(
         libnccl,
         "lib/libnccl.so.2",
@@ -135,12 +130,6 @@ function __init__()
     )
 
     JLLWrappers.@init_library_product(
-        libnvrtc,
-        "lib/libnvrtc.so.12",
-        nothing,
-    )
-
-    JLLWrappers.@init_library_product(
         libnvrtc_builtins,
         "lib/libnvrtc-builtins.so.12.6",
         nothing,
@@ -149,6 +138,17 @@ function __init__()
     JLLWrappers.@init_executable_product(
         fatbinary,
         "lib/cuda/bin/fatbinary",
+    )
+
+    JLLWrappers.@init_file_product(
+        libdevice,
+        "lib/cuda/nvvm/libdevice/libdevice.10.bc",
+    )
+
+    JLLWrappers.@init_library_product(
+        libnvrtc,
+        "lib/libnvrtc.so.12",
+        nothing,
     )
 
     JLLWrappers.@init_executable_product(
